185c34dfb523a588741e38901482e6d1ae0f7b2f
jessdobs <jess.dobs@gmail.com>
2024-02-09 22:00:08 -0500

    add search html add settings html update modal so buttons change depending on what action opened the modal add to do list add settings button and update styling to match search area

--- js/script.js
+++ js/script.js
@@ -23,15 +23,38 @@
 /* Variables */
 let employees;
 let currentEmpIndex;
-let modalContainer = createHtmlElement('div', [{name: 'class', val: 'modal-container hide'}]);
-let modal = createHtmlElement('div', [{name: 'class', val: 'modal'}]);
-let modalContent = createHtmlElement('div', [{name: 'class', val: 'modal-info-container'}]);
-let modalClose = createHtmlElement('button', [{name: 'id', val: 'modal-close-btn'}, {name: 'class', val: 'modal-close-btn'}]);
-let modalNav = createHtmlElement('div', [{name: 'class', val: 'modal-btn-container'}]);
-document.body.insertAdjacentElement('beforeend', modalContainer);
-const searchContainer = document.querySelector('.search-container'); 
 
+/* Variables to hold Elements that will have Event Listeners*/
+const modalContainer = createHtmlElement('div', [
+    {name: 'class', val: 'modal-container hide'}]);
+const modal = createHtmlElement('div', [
+    {name: 'class', val: 'modal'}]);
+const modalContent = createHtmlElement('div', [
+    {name: 'class', val: 'modal-info-container'}]);
+const modalClose = createHtmlElement('button', [
+    {name: 'id', val: 'modal-close-btn'}, {name: 'class', val: 'modal-close-btn'}]);
+const modalBtnContainer = createHtmlElement('div', [
+    {name: 'class', val: 'modal-btn-container'}]);
+const searchInput = createHtmlElement('input', [
+    {name: 'type', val: 'search'}, 
+    {name: 'id', val: 'search-input'}, 
+    {name: 'class', val: 'search-input'}, 
+    {name: 'placeholder', val: 'Search...'}]);
+const searchBtn = createHtmlElement('input', [
+    {name: 'type', val: 'submit'}, 
+    {name: 'id', val: 'search-submit'}, 
+    {name: 'class', val: 'search-submit'}, 
+    {name: 'title', val: 'Search'}, 
+    {name: 'value', val: '\u{1f50d}'}]);
+const settingsBtn = createHtmlElement('button', [
+    {name: 'type', val: 'button'}, 
+    {name: 'id', val: 'settings'}, 
+    {name: 'class', val: 'btn'}]);
+settingsBtn.textContent = '\u{1F354}';
+
+document.body.insertAdjacentElement('beforeend', modalContainer);
 buildModalSkeleton();
+buildActionArea();
 
 // Fetch Functions
 async function fetchUsers() {
@@ -50,25 +73,41 @@ function createHtmlElement(elTag, elAttrs) {
     return elVar;
 }
 
-function buildSearchArea() {
-                            
+function buildActionArea() {
+    const searchContainer = document.querySelector('.search-container');
+    const form = createHtmlElement('form', [{name: 'action', val:'#'}, {name: 'method', val: 'get'}]);
+
+    form.appendChild(searchInput);
+    form.appendChild(searchBtn);
+    form.appendChild(settingsBtn);
+    searchContainer.appendChild(form);
 } 
 
 function buildModalSkeleton() {
     modalContainer.appendChild(modal);
-    modalContainer.appendChild(modalNav);
-
     modal.appendChild(modalClose);
     modal.appendChild(modalContent);
     
     modalClose.textContent = 'X';
+} //doesn't have nav attached so can reuse for settings html
 
-    let navBtns = `
+function addModalBtns(btnType) {
+    modalContainer.appendChild(modalBtnContainer);
+    modalBtnContainer.innerHTML = '';
+    let modalBtns;
+    if (btnType === 'nav') {
+        modalBtns = `
         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
         <button type="button" id="modal-next" class="modal-next btn">Next</button>
     `;
-
-    modalNav.insertAdjacentHTML('beforeend', navBtns);
+    } else if (btnType === 'settings') {
+        modalBtns = `
+        <button type="button" id="modal-cancel" class="modal-cancel btn">Cancel</button>
+        <button type="submit" id="modal-save" class="modal-save btn">Save</button>
+    `;
+    }
+    
+    modalBtnContainer.insertAdjacentHTML('beforeend', modalBtns);
 }
 
 function cacheEmployees(data) {
@@ -81,10 +120,57 @@ function displayEmployeeCards(employees) {
     return employees;
 }
 
-function clearEmployeeDetails() {
+function clearModalContent() {
     modalContent.innerHTML = '';
 }
 
+function displaySettings() {
+    const settingsHtml = `
+        <form>
+            <div class="form-field">
+                <label for="results-count">No. Employess to Fetch:</label>
+                <input type="text" id="results-count" class="results-count" minlength="2" maxlength="3"/>
+            </div>
+            <fieldset>
+                <legend>Select the fields to exclude:</legend>
+                <div>
+                    <input type="checkbox" id="gender" name="gender" value="gender"/>
+                    <label for="gender">Gender</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="login" name="login" value="login"/>
+                    <label for="login">Login</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="registered" name="registered" value="registered"/>
+                    <label for="registered">Registered</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="phone" name="phone" value="phone"/>
+                    <label for="phone">Phone</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="id" name="id" value="id"/>
+                    <label for="id">Id</label>
+                </div>
+            </fieldset>
+        </form>
+    `;
+    
+    modalContent.insertAdjacentHTML('beforeend', settingsHtml);
+    let settingFields = modalContent.querySelectorAll('input');
+    settingFields.forEach(input => {
+        if (input.type === 'checkbox') {
+            if (excludeFields.includes(input.name)) {
+                input.checked = true;
+            }
+        } else if (input.type === 'text') {
+            input.value = userNo;
+        }
+    });
+    
+    //remove checked from the html template and instead look at the excludeFields variable to see what fields should be checked when the modal opens
+}
 // Event Listeners
 gallery.addEventListener('click', e => {
     const target = e.target;
@@ -104,6 +190,7 @@ gallery.addEventListener('click', e => {
         selectedEmp.displayEmployeeDetails();
         currentEmpIndex = employees.indexOf(selectedEmp);
         modalContainer.classList.remove('hide');
+        addModalBtns('nav');
     }
 });
 
@@ -113,15 +200,38 @@ modalClose.addEventListener('click', e => {
 });
 
 modalContainer.addEventListener('click', e => {
-    modalContainer.classList.add('hide');
-    modalContent.innerHTML = '';
+    if (e.target.className === 'modal-container') {
+        modalContainer.classList.add('hide');
+        modalContent.innerHTML = '';
+    }
 });
 
-modalNav.addEventListener('click', e => {
+modalBtnContainer.addEventListener('click', e => {
     const lastEmpIndex = employees.length - 1;
 
-    if (e.target.tagName === 'BUTTON') {
-        clearEmployeeDetails();
+    if (e.target.type === 'submit') {
+        excludeFields = [];
+        employees = [];
+        gallery.innerHTML = '';
+        const inputs = modalContent.querySelectorAll('input');
+
+        inputs.forEach(field => {
+            if (field.type === 'checkbox' && field.checked) {
+                excludeFields.push(field.value);
+                
+            } else if (field.type === 'text' && field.value) {
+                userNo = field.value;
+            }
+            
+        });
+        testing = false;
+        modalContainer.classList.add('hide');
+        modalContent.innerHTML = '';
+        fetchUsers();
+        
+
+    } else if (e.target.tagName === 'BUTTON') {
+        clearModalContent();
 
         if (e.target.id === 'modal-prev') {
             if (currentEmpIndex !== 0) {
@@ -141,10 +251,15 @@ modalNav.addEventListener('click', e => {
                 currentEmpIndex = 0;
                 employees[currentEmpIndex].displayEmployeeDetails();
             }
-        }
+        } 
     }
 });
 
+settingsBtn.addEventListener('click', e => {
+    displaySettings();
+    addModalBtns('settings');
+    modalContainer.classList.remove('hide');
+});
 
 /// below function is just to switch between using test data vs using api call to populate employees
 let testing = true;
@@ -161,3 +276,5 @@ function displayTestEmployees() {
     employees = employees.map(employee => new Employee(employee));
     employees.forEach(employee => employee.displayEmployeeCard());
 };
+
+//to do - style the settings fields; add functionality to the search input and button; all the comments; custom styling; update readme
